6aca9585c9e8e760bd73f03a033dab0fd2d8c113
casellner <114782256+casellner@users.noreply.github.com>
2025-03-06 10:23:34 -0500

    Practice if-else statements

--- Foundational C# with Microsoft/CsharpProjects/TestProject/Program.cs
+++ Foundational C# with Microsoft/CsharpProjects/TestProject/Program.cs
@@ -1,7 +1,23 @@
-﻿int firstValue = 500;
-int secondValue = 600;
-int largerValue;
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
 
-largerValue = System.Math.Max(firstValue, secondValue);
+daysUntilExpiration = 11;
 
-Console.WriteLine(largerValue);
+if (daysUntilExpiration == 0) {
+    Console.WriteLine("Your subscription has expired.");
+} else if (daysUntilExpiration == 1) {
+    Console.WriteLine("Your subscription expires within a day!");
+} else if (daysUntilExpiration <= 5) {
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+} else if (daysUntilExpiration <= 10) {
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+
+if (daysUntilExpiration == 1) {
+    discountPercentage = 20;
+    Console.WriteLine($"Renew now and save {discountPercentage}%!");
+} else if (daysUntilExpiration <= 5 && daysUntilExpiration != 0) {
+    discountPercentage = 10;
+    Console.WriteLine($"Renew now and save {discountPercentage}%!");
+}
--- Foundational C# with Microsoft/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ Foundational C# with Microsoft/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22999dc2f7eb38ec407807cea66f07e8ded2ba5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+546ae14c1db71c3c43ebd208976b9e8933789ec4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
