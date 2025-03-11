11f023af637a07ee59860966b63106abcc2b7d80
casellner <114782256+casellner@users.noreply.github.com>
2025-03-11 12:07:06 -0400

    Practice for loops

--- Foundational C# with Microsoft/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ Foundational C# with Microsoft/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85814b6ae857bcdb091eba80bbc48a4b57e4848b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc5e49291c1fbb3bc177caa875f9fca5b57f59ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
