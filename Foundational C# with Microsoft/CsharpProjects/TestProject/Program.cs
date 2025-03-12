8482a6daf861cd376c1db3fdf5857f3412eb29f2
casellner <114782256+casellner@users.noreply.github.com>
2025-03-11 21:42:03 -0400

    Practice do-while and while statements

--- Foundational C# with Microsoft/CsharpProjects/TestProject/Program.cs
+++ Foundational C# with Microsoft/CsharpProjects/TestProject/Program.cs
@@ -1,12 +1,73 @@
-﻿for (int i = 1; i <= 100; i++)
-{
-    Console.Write(i);
-    if (i % 3 == 0 || i % 5 == 0) {
-        Console.Write(" - ");
-        if (i % 3 == 0)
-            Console.Write("Fizz");
-        if (i % 5 == 0)
-            Console.Write("Buzz");
+﻿/*
+string? readResult;
+bool validEntry = false;
+bool inRange = false;
+int numericEntry;
+
+Console.WriteLine("Enter an integer value between 5 and 10");
+
+do {
+    readResult = Console.ReadLine();
+    validEntry = int.TryParse(readResult, out numericEntry);
+
+    if (validEntry) {
+        if (numericEntry >= 5 && numericEntry <= 10) {
+            inRange = true;
+            Console.WriteLine($"Your input value ({numericEntry}) has been accepted.");
+        } else {
+            Console.WriteLine($"You entered {numericEntry}. Please enter a number between 5 and 10.");
+        }
+    } else {
+        Console.WriteLine("Sorry, you entered an invalid number, please try again");
     }
-    Console.Write("\n");
+} while (inRange == false);
+*/
+
+/*
+string? readResult;
+string parsedResult = "";
+bool matches = false;
+
+Console.WriteLine("Enter your role name (Administrator, Manager, or User)");
+
+do {
+    readResult = Console.ReadLine();
+    if (readResult != null) {
+        parsedResult = readResult.Trim().ToLower();
+    }
+    switch (parsedResult) {
+        case "administrator":
+            matches = true;
+            break;
+        case "manager":
+            matches = true;
+            break;
+        case "user":
+            matches = true;
+            break;
+        default:
+            Console.WriteLine($"The role name that you entered, \"{readResult}\" is not valid. Enter your role name (Administrator, Manager, or User)");
+            break;
+    }
+} while (matches == false);
+
+Console.WriteLine($"Your input value ({readResult}) has been accepted.");
+*/
+
+string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
+string myString;
+int periodLocation;
+string sentence;
+
+for (int i = 0; i < myStrings.Length; i++) {
+    myString = myStrings[i];
+    periodLocation = myString.IndexOf(".");
+    
+    while (periodLocation != -1) {
+        sentence = myString.Substring(0, periodLocation);
+        myString = myString.Remove(0, periodLocation + 1).TrimStart();
+        Console.WriteLine(sentence);
+        periodLocation = myString.IndexOf(".");
+    }
+    Console.WriteLine(myString);
 }
--- Foundational C# with Microsoft/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ Foundational C# with Microsoft/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc5e49291c1fbb3bc177caa875f9fca5b57f59ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11f023af637a07ee59860966b63106abcc2b7d80")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
