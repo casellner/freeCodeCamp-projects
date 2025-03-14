3dd5814fcfd1c5e397f3f67448d29dd6e38f741c
casellner <114782256+casellner@users.noreply.github.com>
2025-03-14 00:03:04 -0400

    Practice casting and conversion techniques

--- Foundational C# with Microsoft/CsharpProjects/TestProject/Program.cs
+++ Foundational C# with Microsoft/CsharpProjects/TestProject/Program.cs
@@ -1,44 +1,103 @@
-﻿
-Console.WriteLine("Signed integral types:");
-
-Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
-Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
-Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
-Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
-
-Console.WriteLine("");
-Console.WriteLine("Unsigned integral types:");
-
-Console.WriteLine($"byte   : {byte.MinValue} to {byte.MaxValue}");
-Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
-Console.WriteLine($"uint   : {uint.MinValue} to {uint.MaxValue}");
-Console.WriteLine($"ulong  : {ulong.MinValue} to {ulong.MaxValue}");
-
-/*
-Console.WriteLine("");
-Console.WriteLine("Floating point types:");
-Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
-Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
-Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
+﻿/*
+int first = 2;
+string second = "4";
+string result = first + second;
+Console.WriteLine(result);
 */
 /*
-int[] data = new int[3];
+int myInt = 3;
+Console.WriteLine($"int: {myInt}");
 
-string shortenedString = "Hello World!";
-Console.WriteLine(shortenedString + "\n");
-
-int val_A = 2;
-int val_B = val_A;
-val_B = 5;
-Console.WriteLine("--Value Types--");
-Console.WriteLine($"val_A: {val_A}");
-Console.WriteLine($"val_B: {val_B}\n");
-
-int[] ref_A= new int[1];
-ref_A[0] = 2;
-int[] ref_B = ref_A;
-ref_B[0] = 5;
-Console.WriteLine("--Reference Types--");
-Console.WriteLine($"ref_A[0]: {ref_A[0]}");
-Console.WriteLine($"ref_B[0]: {ref_B[0]}\n");
+decimal myDecimal = myInt;
+Console.WriteLine($"decimal: {myDecimal}");
 */
+/*
+decimal myDecimal = 3.14m;
+Console.WriteLine($"decimal: {myDecimal}");
+
+int myInt = (int)myDecimal;
+Console.WriteLine($"int: {myInt}");
+*/
+/*
+decimal myDecimal = 1.23456789m;
+float myFloat = (float)myDecimal;
+
+Console.WriteLine($"Decimal: {myDecimal}");
+Console.WriteLine($"Float  : {myFloat}");
+*/
+/*
+int first = 5;
+int second = 7;
+string message = first.ToString() + second.ToString();
+Console.WriteLine(message);
+*/
+/*
+string first = "5";
+string second = "7";
+int sum = int.Parse(first) + int.Parse(second);
+Console.WriteLine(sum);
+*/
+/*
+string value1 = "5";
+string value2 = "7";
+int result = Convert.ToInt32(value1) * Convert.ToInt32(value2);
+Console.WriteLine(result);
+*/
+/*
+int value = (int)1.5m; //truncate
+Console.WriteLine(value);
+
+int value2 = Convert.ToInt32(1.5m); //round up
+Console.WriteLine(value2);
+*/
+/*
+string value = "bad";
+int result = 0;
+if (int.TryParse(value, out result))
+{
+    Console.WriteLine($"Measurement: {result}");
+}
+else
+{
+    Console.WriteLine("Unable to report the measurement");
+}
+if (result > 0)
+    Console.WriteLine($"Measurement (w/ offset): {50 + result}");
+*/
+/*
+string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+float value = 0.0f;
+float total = 0;
+string message = "";
+
+for (int i = 0; i < values.Length; i++)
+{
+    if (float.TryParse(values[i], out value))
+    {
+        total += value;
+    }
+    else
+    {
+        message += values[i];
+    }
+}
+
+Console.WriteLine($"Message: {message}");
+Console.WriteLine($"Total: {total}");
+*/
+
+int value1 = 11;
+decimal value2 = 6.2m;
+float value3 = 4.3f;
+
+int result1 = Convert.ToInt32(value1 / value2);
+Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+
+decimal result2 = value2 / (decimal)value3;
+Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+
+float result3 = value3 / value1;
+Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
+
+string test = "4.12";
+decimal test2 = (decimal)test;
--- Foundational C# with Microsoft/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ Foundational C# with Microsoft/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3be5438191500fbb3823b606c1da56f02c7d46b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03409f595b0d9dd4796f0c79a1b33b4526647597")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
