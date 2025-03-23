576927e0a00fca38c88b451a7ace674458dff80c
casellner <114782256+casellner@users.noreply.github.com>
2025-03-23 01:25:25 -0400

    Practice C# Exception Handling

--- Foundational C# with Microsoft/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ Foundational C# with Microsoft/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f463c30beafdc29ec2e44a53a106ec514fa62070")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b44be15ad186ae4182da4c4a09e3746669a3e780")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
