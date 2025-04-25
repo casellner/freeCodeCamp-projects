099ac374cb0dc9f1860e5d83732311245e851ff3
casellner <114782256+casellner@users.noreply.github.com>
2025-04-25 15:22:16 -0400

    Clean up C# code

--- Foundational C# with Microsoft/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ Foundational C# with Microsoft/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+576927e0a00fca38c88b451a7ace674458dff80c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+890cc91c056f2685933cc2ba93d5883a9bdb3040")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
